0200b2a6b047a989a7caf6748e67cdb960e8a85f
shshzaa <shshzaa@gmai.com>
2013-11-04 11:03:00 +0000

    change the sentense

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var file = require('fs');
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
